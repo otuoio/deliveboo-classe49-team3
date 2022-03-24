c457d3562def275985080391b7b1845957bed468
otuoio <mariocoppo91@gmail.com>
2022-03-24 12:19:21 +0100

    add user migration and model on dev

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -16,8 +17,43 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'slug',
+        'address',
+        'p_iva',
+        'phone_number',
+        'shipment_price',
+        'minimum order',
+        'image'
     ];
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function createSlug($name)
+    {
+        $slug = Str::slug($name, '-');
+
+        $oldUser = User::where('slug', $slug)->first();
+
+        $counter = 0;
+        while ($oldUser) {
+            $newSlug = $slug . '-' . $counter;
+            $oldUser = User::where('slug', $newSlug)->first();
+            $counter++;
+        }
+
+        return (empty($newSlug)) ? $slug : $newSlug;
+    }
 
     /**
      * The attributes that should be hidden for arrays.
