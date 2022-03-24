c4a3e9c6d26759dc126bc054e6bda50b1d784ef5
saragrigolin <sara.grigolin95@gmail.com>
2022-03-24 14:37:41 +0100

    create migration dishes

---
+++ database/migrations/2022_03_24_115537_create_dishes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200)->unique();
+            $table->string('image', 255)->nullable();
+            $table->text('description')->nullable();
+            $table->float('price', 6, 2);
+            $table->string('slug', 255)->unique();
+            $table->boolean('visible');
+            $table->boolean('vegan');
+            $table->boolean('spicy');
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
