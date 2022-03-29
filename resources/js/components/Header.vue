d4c022a6ad2b9ad1d884362643ff856834a3b286
otuoio <mariocoppo91@gmail.com>
2022-03-29 15:35:09 +0200

    add header home

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        Ciao sono l'header
-    </div>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                </a>
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                </form>
+            </div>
+        </div>
+  </header>
 </template>
 
 <script>
