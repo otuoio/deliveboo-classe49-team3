f710f1f33cf269a1ee36b1eaa31fad3132f5b093
saragrigolin <sara.grigolin95@gmail.com>
2022-03-30 09:55:32 +0200

    push js test

--- public/js/admin.js
+++ public/js/admin.js
@@ -1212,8 +1212,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/mariocoppola/boolean/php/deliveboo-classe49-team3/resources/js/admin.js */"./resources/js/admin.js");
-module.exports = __webpack_require__(/*! /Users/mariocoppola/boolean/php/deliveboo-classe49-team3/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/saragrigolin/Boolean/Esercizi_php/deliveboo-classe49-team3/resources/js/admin.js */"./resources/js/admin.js");
+module.exports = __webpack_require__(/*! /Users/saragrigolin/Boolean/Esercizi_php/deliveboo-classe49-team3/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
