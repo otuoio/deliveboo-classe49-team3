6acd71a01bedc951c790b41b068ccb575251636a
saragrigolin <sara.grigolin95@gmail.com>
2022-04-05 19:36:25 +0200

    add orders to db

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\Order;
-
+use App\Model\Dish;
+use Carbon\Carbon;
 class OrderController extends Controller
 {
     public function orderDatas(){
@@ -17,11 +18,33 @@ class OrderController extends Controller
         ]);
     }
     public function sendOrder(Request $request) {
-        $data = $request->params['info'];
-        // return view('guest.success', ['data'=>$data]);
+        $orderInfo = $request->params['info'];
+        $userCart = $request->params['info']['cartDishes'];
+        // $dishes = Dish::all();
+
+        $todayIstance = new Carbon();
+        $today = $todayIstance->now();
+        $todayDate = $today->format('Y-m-d');
+        $todayTime = $today->format('H:i:s');
+        
+
+        $newOrder = new Order();
+        $newOrder->customer_name = $orderInfo['customer_name'];
+        $newOrder->phone_number = $orderInfo['phone_number'];
+        $newOrder->address = $orderInfo['address'];
+        $newOrder->email = $orderInfo['email'];
+        $newOrder->total = $orderInfo['cartTotal'];
+        $newOrder->date = $todayDate;
+        $newOrder->time = $todayTime;
+        $newOrder->save();
+
+        foreach ($userCart as $element) {
+            $newOrder->dishes()->attach($element['id'], ['qty' => $element['quantity']]);
+        }
+
         return response()->json([
             "success" => true,
-            "results" => $data,
+            "results" => $newOrder,
         ]);
     }
 }
