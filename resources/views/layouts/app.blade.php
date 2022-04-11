8f4e765ebbdac102703e3da684b639a021d62b1c
saragrigolin <sara.grigolin95@gmail.com>
2022-04-11 09:25:44 +0200

    fine davvero

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('documentTitle')</title>
+    <title>Deliveboo</title>
 
     <!-- Scripts -->
     @yield('script')
@@ -19,6 +20,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <main>
@@ -26,4 +28,5 @@
         </main>
     </div>
 </body>
+
 </html>
